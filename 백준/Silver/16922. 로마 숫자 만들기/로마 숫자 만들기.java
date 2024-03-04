f4698e12875390bf35e31a9625e2674c4464c280
Hugi <81164267+Huggy2646@users.noreply.github.com>
2024-03-04 16:55:30 +0900

    [Silver III] Title: 로마 숫자 만들기, Time: 124 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Silver/16922. 로마 숫자 만들기/로마 숫자 만들기.java
+++ 백준/Silver/16922. 로마 숫자 만들기/로마 숫자 만들기.java
@@ -7,13 +7,16 @@ import java.util.Set;
 public class Main {
 	static int N;
 	static int result;
+    // 각각 l V X L
 	static int [] roma= {1,5,10,50};
+	// 결과로 나오는 값들을 set으로 저장하여 중복 값 제거
 	static Set<Integer> set =new HashSet<>();
 	public static void dfs(int level,int start,int buffer) {
 		if(level==N) {
 			set.add(buffer);
 			return;
 		}
+		//중복조합
 		for(int i=start; i<4; i++) {
 			dfs(level+1,i,buffer+roma[i]);
 		}
@@ -23,7 +26,7 @@ public class Main {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N=Integer.parseInt(br.readLine());
-		
+		//중복조합
 		dfs(0,0,0);
 		System.out.println(set.size());
 	}
