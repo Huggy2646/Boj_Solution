21e8474d7e8bfe2287e9adf476ac4f983adfc4e2
Hugi <81164267+Huggy2646@users.noreply.github.com>
2024-03-04 15:50:17 +0900

    [Silver III] Title: 근손실, Time: 136 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Silver/18429. 근손실/근손실.java
+++ 백준/Silver/18429. 근손실/근손실.java
@@ -9,10 +9,14 @@ public class Main {
 	static int K;
 	static int total_count=0;
 	static boolean[] visited;
+	// 굳이 3대 운동 중량 500계산을 하지 않았음 
+	// p는 현재의 중량 증가된 결과
 	public static void dfs(int level, int p) {
+		//현재 중량 증가량이 -1가 된다면 운동을 해도 근손실이 더 크다는 뜻이라 가지치기
 		if(p<0) {
 			return;
 		}
+		// 가지치기 안되고 끝까지 갔다면 유지가 잘 된거라서 count
 		if(level==N) {
 			total_count++;
 			return;
@@ -20,6 +24,7 @@ public class Main {
 		for(int i=0; i<N; i++) {
 			if(!visited[i]) {
 				visited[i]=true;
+				//넘겨 줄 때 현재 중량 + 증가될 증량 - 근손실
 				dfs(level+1,p+powers[i]-K);
 				visited[i]=false;
 			}
@@ -30,14 +35,17 @@ public class Main {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		//데이터 init
 		N = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
-		st= new StringTokenizer(br.readLine()," ");
 		powers=new int[N];
 		visited=new boolean[N];
+		st= new StringTokenizer(br.readLine()," ");
 		for(int i=0; i<N; i++) {
 			powers[i]=Integer.parseInt(st.nextToken());
 		}
+		
+		//dfs시작(순열)
 		dfs(0,0);
 		System.out.println(total_count);
 		
