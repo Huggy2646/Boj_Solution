a1a7f6857955aa681744c4c7a34079b4ec7a6efa
Hugi <81164267+Huggy2646@users.noreply.github.com>
2024-04-10 17:57:37 +0900

    [Unrated] Title: [모의 SW 역량테스트] 숫자 만들기, Time: 128 ms, Memory: 26,676 KB -BaekjoonHub

--- SWEA/Unrated/4008. ［모의 SW 역량테스트］ 숫자 만들기/［모의 SW 역량테스트］ 숫자 만들기.java
+++ SWEA/Unrated/4008. ［모의 SW 역량테스트］ 숫자 만들기/［모의 SW 역량테스트］ 숫자 만들기.java
@@ -1,81 +1,83 @@
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Solution {
-	static String [] opers= {"+","-","*","/"};
-	static int [] opers_count;
-	static boolean[]visited;
-	static int N;
-	static int[] result;
-	static int []numbers;
-	static int max = Integer.MIN_VALUE;
-	static int min= Integer.MAX_VALUE;
-	public static void permu(int level) {
-		if(level==N-1) {
-			int operIndex=0;
-			int buffer=numbers[0];			
-			for(int i=1; i<N; i++) {
-				int oper =result[operIndex];
-				if(oper==0)
-					buffer+=numbers[i];
-				else if(oper==1)
-					buffer-=numbers[i];
-				else if(oper==2)
-					buffer*=numbers[i];
-				else
-					buffer/=numbers[i];
-				operIndex++;
-			}
-			max=max<buffer? buffer:max;
-			min=buffer<min? buffer:min;
-			return;
-		}
-		for(int i=0; i<opers_count.length; i++) {
-			if(opers_count[i]!=0) {
-				opers_count[i]--;
-				result[level]=i;
-				permu(level+1);
-				opers_count[i]++;
-
-			}
-		}
-
-
-	}
+	static int [] oper_result;
+	private static int[] opers;
+	private static int N;
+	private static int cnt=0;
+	private static int[] nums;
+	private static int result;
+	private static int max;
+	private static int min;
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		// TODO Auto-generated method stub
-		//System.setIn(new FileInputStream("res/sample_input.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
 		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-
-		int T =Integer.parseInt(br.readLine());
 		for(int t=0; t<T; t++) {
 			N = Integer.parseInt(br.readLine());
-			
-			visited=new boolean[N-1];
-			result=new int[N-1];
-			opers_count=new int [N-1];
-			opers_count=Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-			numbers=new int[N];
-			st=new StringTokenizer(br.readLine()," ");
-			for(int n=0; n<N; n++) {
-				numbers[n]=Integer.parseInt(st.nextToken());
-			}
-			permu(0);
-			sb.append("#").append(t+1).append(" ").append(max-min).append("\n");
+			opers = new int [4];
+			oper_result=new int[N-1];
+			nums = new int [N];
 			max=Integer.MIN_VALUE;
 			min=Integer.MAX_VALUE;
+			StringTokenizer st = new StringTokenizer(br.readLine()," ");
+			for(int i=0; i<4; i++) {
+				opers[i]=Integer.parseInt(st.nextToken());
 			}
+			st = new StringTokenizer(br.readLine()," ");
+			for(int n=0;n<N;n++) {
+				nums[n]=Integer.parseInt(st.nextToken());
+			}
+			
+			permu(0);
 
-		
-			System.out.println(sb);
+			sb.append("#").append(t+1).append(" ").append(max-min).append("\n");
+		}
+		System.out.println(sb);
+	}
+
+	private static void permu(int level) {
+		if(level==N-1) {
+			calc();
+
+		}
+		for(int i=0; i<4; i++) {
+			if(opers[i]!=0) {
+				oper_result[level]=i;
+				opers[i]--;
+				permu(level+1);
+				opers[i]++;
+			}
+			
 		}
 		
+	}
+
+	private static void calc() {
+		result=nums[0];
+		for(int i=0; i<N-1; i++) {
+			// +
+			if(oper_result[i]==0) {
+				result+=nums[i+1];
+			}
+			// -
+			else if(oper_result[i]==1) {
+				result-=nums[i+1];
+			}
+			// *
+			else if(oper_result[i]==2) {
+				result*=nums[i+1];
+			}
+			// /
+			else
+				result=(int)(result/nums[i+1]);
+		}
+		max = Integer.max(result, max);
+		min = Integer.min(result,min);
+		
+	}
 }
