851e55fbe2cd50528204845fcc77f4b3fb609c79
Hugi <81164267+Huggy2646@users.noreply.github.com>
2025-07-09 23:15:22 +0900

    [Silver IV] Title: solved.ac, Time: 332 ms, Memory: 39128 KB -BaekjoonHub

---
+++ 백준/Silver/18110. solved．ac/solved．ac.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		if(n == 0) {
+			System.out.println(0);
+			return;
+		} else if(n==1) {
+			System.out.println(br.readLine());
+			return;
+		} else if(n==2) {
+			System.out.println(Math.round(Integer.parseInt(br.readLine())+Integer.parseInt(br.readLine()))/2);
+			return;
+		}else if(n==3) {
+			System.out.println(Math.round(Integer.parseInt(br.readLine())+Integer.parseInt(br.readLine())+Integer.parseInt(br.readLine()))/3);
+			return;
+			
+		}
+		
+		int ftper = (int) Math.round(n*0.15);
+		int[] nums = new int[n];
+		for(int i=0; i<n; i++) {
+			nums[i] = Integer.parseInt(br.readLine());
+		}
+		
+		Arrays.sort(nums);
+		int sum = 0;
+		int cnt = 0;
+		for(int i=ftper; i<n-ftper; i++) {
+			sum+=nums[i];
+			cnt++;
+		}
+		System.out.println(Math.round((float)sum/cnt));
+		
+		return ;
+	}
+
+}
